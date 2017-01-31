4d92043c35784225ff74b9b485522b200d972ab8
Phi Long Nguyen <philong.nguyen95@gmail.com>
2017-01-30 21:37:39 -0500

    updated worker's method
    added get and set manager

--- src/cd/entity/Worker.java
+++ src/cd/entity/Worker.java
@@ -3,5 +3,13 @@ package cd.entity;
 import cd.CDException;
 
 public interface Worker extends User {
-
+    /** Return the worker's manager.
+     * @return the worker's manager
+     */
+	public Manager getManager();
+	
+	/** Set the worker's manager.
+     * @param manager the new manager of this worker
+     */
+	public void setManager(Manager manager);
 }
