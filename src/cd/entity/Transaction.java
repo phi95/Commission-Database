8fd9c4fe0e0d219a06a24d63c19108ebf46c1714
Quanzu <Dhn74201@uga.edu>
2017-01-30 21:24:59 -0500

    Started transaction

--- src/cd/entity/Transaction.java
+++ src/cd/entity/Transaction.java
@@ -2,7 +2,13 @@ package cd.entity;
 
 import cd.CDException;
 import cd.persistence.Persistable;
+import java.util.Date;
+public interface Transaction extends Persistable {	
 
-public interface Transaction extends Persistable {
-
+	/**
+	 * Retrieves the date of the transaction.
+	 * @return the transaction's date.
+	 */
+	public Date getDate();
+	
 }
