e6cd2e44876890d8ccfc9654e549cea9d3b41390
Phi Long <philong.nguyen95@gmail.com>
2017-02-13 23:13:00 -0500

    documented the logic layer

--- src/cd/logic/LogicLayer.java
+++ src/cd/logic/LogicLayer.java
@@ -5,13 +5,29 @@ import cd.session.Session;
 
 public interface LogicLayer {
 
+	/**
+	 * Logs into the system as an employer
+	 * @return the ssid of the employer.
+	 */
 	String employerLogin(Session session, String username, String password);
 
+	/**
+	 * Logs into the system as an employee
+	 * @return ssid of the employee.
+	 */
 	String employeeLogin(Session session, String username, String password);
 
+	/**
+	 * Register an employer
+	 * @return ssid of the new employer.
+	 */
 	String addEmployer(Session session, String firstName, String lastName, String username, String password,
 			String email, String phoneNumber);
 
+	/**
+	 * Register an employee.
+	 * @return ssid of the new employee.
+	 */
 	String addEmployee(Session session, String firstName, String lastName, String username, String password,
 			String email, String phoneNumber);
 
