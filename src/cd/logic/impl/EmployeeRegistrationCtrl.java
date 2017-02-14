39f531f0029b6098cfc138b00a3d5460bd7abfc3
Dustin-Local <dhn74201@uga.edu>
2017-02-14 01:01:49 -0500

    Registration Implementation

--- src/cd/logic/impl/EmployeeRegistrationCtrl.java
+++ src/cd/logic/impl/EmployeeRegistrationCtrl.java
@@ -1,5 +1,45 @@
 package cd.logic.impl;
 
+
+
+import java.sql.Connection;
+import java.util.List;
+
+import cd.CDException;
+import cd.object.*;
+import cd.session.*;
+import cd.entity.*;
+
+
 public class EmployeeRegistrationCtrl {
 
-}
+	private ObjectLayer objectLayer = null;
+	
+	public EmployeeRegistrationCtrl(ObjectLayer objectLayer) {
+		this.objectLayer = objectLayer;
+	} // EmployeeRegistrationCtrl
+	
+	
+	
+	public String addEmployee(Session session, String fname, String lname, String userName, String password, String email, String phoneNumber) throws CDException {
+		String ssid = null;
+		
+		// PLAN: TO REPLACE WITH! ONLY EMPLOYERS CAN REGISTER EMPLOYEES!
+		
+		// Check if username already exists.
+		Employee employee = objectLayer.createEmployee();
+		employee.setUserName(userName);
+		List<Employee> employeesWithUsername = objectLayer.findEmployee(employee);
+		if (!employeesWithUsername.isEmpty()) throw new CDException("A person with this username already exists.");
+		
+		// Create user if username is unique.
+		employee = objectLayer.createEmployee(fname, lname, userName, password, email, phoneNumber);
+		objectLayer.storeEmployee(employee);
+		
+		// Session handling
+		session.setUser(employee);
+		ssid = SessionManager.storeSession(session);
+		return ssid;
+	} // addEmployee
+	
+} // EmployeeRegistrationCtrl
--- src/cd/logic/impl/EmployerRegistrationCtrl.java
+++ src/cd/logic/impl/EmployerRegistrationCtrl.java
@@ -1,5 +1,43 @@
 package cd.logic.impl;
 
+import java.util.List;
+
+import cd.CDException;
+import cd.entity.Employee;
+import cd.entity.Employer;
+import cd.object.ObjectLayer;
+import cd.session.Session;
+import cd.session.SessionManager;
+
 public class EmployerRegistrationCtrl {
 
+	private ObjectLayer objectLayer = null;
+	
+	public EmployerRegistrationCtrl(ObjectLayer objectLayer) {
+		this.objectLayer = objectLayer;
+	} // EmployeeRegistrationCtrl
+	
+	
+	
+	public String addEmployer(Session session, String fname, String lname, String userName, String password, String email, String phoneNumber) throws CDException {
+		String ssid = null;
+		
+		// Check if username already exists.
+		Employer employer = objectLayer.createEmployer();
+		employer.setUserName(userName);
+		List<Employer> employersWithUsername = objectLayer.findEmployer(employer);
+		if (!employersWithUsername.isEmpty()) throw new CDException("A person with this username already exists.");
+		
+		// Create user if username is unique.
+		employer = objectLayer.createEmployer(fname, lname, userName, password, email, phoneNumber);
+		objectLayer.storeEmployer(employer);
+		
+		// Session handling
+		session.setUser(employer);
+		ssid = SessionManager.storeSession(session);
+		return ssid;
+	} // addEmployee
+
+	
+	
 }
